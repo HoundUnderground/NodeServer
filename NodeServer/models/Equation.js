dc733ed677fe7f643731985f5d084a8b84b54937
HoundUnderground <30447915+HoundUnderground@users.noreply.github.com>
2025-04-27 18:34:39 -0400

    Delete NodeServer directory

--- NodeServer/models/Equation.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const EquationSchema = new mongoose.Schema({
-  expression: { type: String, required: true },
-  answer: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Equation', EquationSchema);
